5a95a6db928e5907206ed778eb20a0795fe8eab7
shristyzrg <shristyzrg@gmail.com>
2016-11-04 09:53:31 -0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,38 @@
+jQuery(function($) {
+
+	//#main-slider
+
+	$( '.centered' ).each(function( e ) {
+		$(this).css('margin-top',  ($('#main-slider').height() - $(this).height())/2);
+	});
+
+	$(window).resize(function(){
+		$( '.centered' ).each(function( e ) {
+			$(this).css('margin-top',  ($('#main-slider').height() - $(this).height())/2);
+		});
+	});
+
+	
+   
+
+
+	
+
+	//goto top
+	$('.gototop').click(function(event) {
+		event.preventDefault();
+		$('html, body').animate({
+			scrollTop: $("body").offset().top
+		}, 500);
+	});	
+
+	//Pretty Photo
+	$("a[rel^='prettyPhoto']").prettyPhoto({
+		social_tools: false
+	});	
+    
+    //modal window
+       $("#myBtn").click(function(){
+        $("#myModal").modal();
+    });
+});
